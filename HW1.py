54027df193c14086a5c696bddc67cad8d6a8dbb1
PrestonBidwell5thHour <pb125344@student.wpsok.org>
2024-08-19 13:09:21 -0500

    added hw2

---
+++ HW1.py
@@ -0,0 +1,4 @@
+#Name: Preston B
+#Class: 5th Hour
+#assignment: HW1
+print ("Hello World")
---
+++ HW2.py
@@ -0,0 +1,13 @@
+#Name: Preston Bidwell
+#Assignment: HW2
+print ("Hello World!")
+
+print("what is your name")
+input()
+
+x = int(input("pick a number"))
+y = int(input("pick a different number"))
+
+sum= x + y
+
+print("the outcome is", sum)
