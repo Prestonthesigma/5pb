60334217e000115a3ffc9839a371fec2a9d8ec33
PrestonBidwell5thHour <pb125344@student.wpsok.org>
2024-08-29 13:18:05 -0500

    playground

---
+++ hw3.py
@@ -0,0 +1,16 @@
+#Preston Bidwell
+#5th hour
+#HW3
+
+print("hello world")
+naughtylist=["preston", "quinn", "Ethan", "gabe", "julian"]
+print(naughtylist)
+naughtylist.append(input())
+print(f"{naughtylist[3]}")
+numList = [1,2,3,4]
+numList.insert(1, 3)
+print (numList)
+numList.sort()
+print (numList)
+sum = numList[0] + numList[1] + numList[2]
+print(sum)
