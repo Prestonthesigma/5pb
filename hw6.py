a723a9217f1437b2cedc0ce712e3f6322f7dbe3d
PrestonBidwell5thHour <pb125344@student.wpsok.org>
2024-10-04 13:17:16 -0500

    HW6

--- hw6.py
+++ hw6.py
@@ -2,16 +2,37 @@
 #5th Hour
 #HW6
 
-print("hello world")
+print ("hello world")
 
-number = int(input("enter a number:"))
+number = 10
 if number % 2 == 0:
-    print(number % 2 == 0)
-    print ("number is divisible by 2.")
+    if number % 3 == 0:
+        print(number/2)
+        print(number/3)
+    else:
+        print("number is not divisible by 3.")
+        print(number/2)
 else:
+    if number % 3 == 0:
         print("number is not divisible by 2.")
-if number % 3 == 0:
-    print(number % 3 == 0)
-    print("number is divisible by 3.")
-else:
-    print("number is not divisible by 3.")
+        print(number/3)
+    else:
+        print("number is divisible by neither 2 nor 3.")
+
+
+
+
+
+#If num is divisible by 2
+#   if num is divisible by 3
+#       print the result of num being divided by 2
+#       print the result of num being divided by 3
+#   else
+#       print that it is not divisible by 3
+#       print the result of num being divided by 2
+#else
+#   if num is divisible by 3
+#       print that num is not divisible by 2
+#       print the result of num being divided by 3
+#   else
+#       print that neither is divisible by 2 or 3
