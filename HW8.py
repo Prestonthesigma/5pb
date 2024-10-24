d48c341685817f4d16b63766244e81df0cb067e6
PrestonBidwell5thHour <pb125344@student.wpsok.org>
2024-10-24 13:08:32 -0500

    HW8

---
+++ HW8.py
@@ -0,0 +1,38 @@
+#Preston
+#5th Hour
+#HW8
+
+intVar1 = 4
+strVar3 = "Box"
+boolVar4 = True
+strVar5 = "Nineteen"
+strVar6 = "Rope"
+intVar7 = 19
+strVar8 = "19"
+listVar9 = ["Window", "Apple", "Penguin"]
+intVar10 = 2
+boolVar11 = True
+boolVar12 = False
+strVar13 = "True"
+strVar14 = "False"
+listVar15 = [1, 0]
+listVar16 = [1]
+listVar17 = [0]
+intVar18 = 5
+strVar19 = "The"
+strVar20 = "Game"
+intVar21 = 3
+intVar22 = 2
+intVar23 = 1
+listVar24 = ["Liftoff", True]
+boolVar25 = boolVar11
+boolVar26 = boolVar12
+strVar27 = strVar13
+strVar28 = strVar14
+listVar29 = listVar15
+intVar30 = 6
+
+
+intVar1 + intVar7 + intVar10 + intVar18 + intVar21 + intVar22 + intVar23 + intVar30
+sum = intVar7 + intVar10 + intVar18 + intVar21 + intVar22 + intVar23 + intVar30
+print (sum)
