f0e6962da554e0e7610a956e42abaf006f44927a
PrestonBidwell5thHour <pb125344@student.wpsok.org>
2024-10-24 13:08:04 -0500

    HW9

---
+++ HW9.py
@@ -0,0 +1,18 @@
+#Preston
+#5th Hour
+#HW9
+
+import random
+
+temp = random.randint(0,20)
+
+
+if temp >= 20:
+    print("It's hot")
+elif temp > 10:
+    print("It's mild")
+else:
+    print("it's cold")
+    print("thank you")
+
+print(temp)
